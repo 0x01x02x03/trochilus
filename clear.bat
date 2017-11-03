96ccf863a550bcc1ec9ffd4c42c6583382399c90
floyd419 <floyd419@foxmail.com>
2017-11-03 16:30:13 +0800

    delete

--- clear.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-del C:\ods.log
-sc stop medialoader
-sc delete medialoader
-
-del %ALLUSERSPROFILE%\Medialoader\ser.dat
-del %ALLUSERSPROFILE%\Medialoader\shell.dll
-del %ALLUSERSPROFILE%\Medialoader\data.dat
